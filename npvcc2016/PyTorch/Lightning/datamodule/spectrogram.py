7582e5b333e9d21747d153b023e77c1d8f2b7bff
Tarepan <tarepan5884@gmail.com>
2020-12-20 14:34:28 +0000

    Add latest dataset

--- npvcc2016/PyTorch/Lightning/datamodule/spectrogram.py
+++ npvcc2016/PyTorch/Lightning/datamodule/spectrogram.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Union
+from typing import Callable, List, Optional, Union
 
 import pytorch_lightning as pl
 from torch.tensor import Tensor
@@ -20,6 +20,9 @@ class NpVCC2016_spec_DataModule(pl.LightningDataModule):
         dir_root: str = "./data/",
         speakers: List[Speaker] = ["SF1", "SM1", "TF2", "TM3"],
         transform: Callable[[Tensor], Tensor] = lambda i: i,
+        corpus_adress: Optional[str] = None,
+        dataset_adress: str = "./data/datasets/npVCC2016_spec/archive/dataset.zip",
+        zipfs: bool = False,
     ):
         super().__init__()
         self.n_batch = batch_size
@@ -27,6 +30,9 @@ class NpVCC2016_spec_DataModule(pl.LightningDataModule):
         self.dir_root = dir_root
         self.speakers = speakers
         self.transform = transform
+        self.corpus_adress = corpus_adress
+        self.dataset_adress = dataset_adress
+        self.zipfs = zipfs
         # transforms.Compose([transforms.ToTensor()])
 
         # self.dims is returned when you call dm.size()
@@ -35,17 +41,39 @@ class NpVCC2016_spec_DataModule(pl.LightningDataModule):
         # self.dims = (1, 28, 28)
 
     def prepare_data(self, *args, **kwargs) -> None:
-        NpVCC2016_spec(train=True, download_corpus=self.download, dir_data=self.dir_root)
+        pass
 
     def setup(self, stage: Union[str, None] = None) -> None:
         if stage == "fit" or stage is None:
-            dataset_train = NpVCC2016_spec(train=True, transform=self.transform, dir_data=self.dir_root)
+            dataset_train = NpVCC2016_spec(
+                train=True,
+                speakers=self.speakers,
+                transform=self.transform,
+                download_corpus=self.download,
+                dir_data=self.dir_root,
+                corpus_adress=self.corpus_adress,
+                dataset_adress=self.dataset_adress,
+                zipfs=self.zipfs,
+                compression=False,
+                cache=False
+            )
             n_train = len(dataset_train)
             self.data_train, self.data_val = random_split(
                 dataset_train, [n_train - 10, 10]
             )
         if stage == "test" or stage is None:
-            self.data_test = NpVCC2016_spec(train=False, transform=self.transform, dir_data=self.dir_root)
+            self.data_test = NpVCC2016_spec(
+                train=False,
+                speakers=self.speakers,
+                transform=self.transform,
+                download_corpus=self.download,
+                dir_data=self.dir_root,
+                corpus_adress=self.corpus_adress,
+                dataset_adress=self.dataset_adress,
+                zipfs=self.zipfs,
+                compression=False,
+                cache=False
+            )
 
     def train_dataloader(self, *args, **kwargs):
         return DataLoader(self.data_train, batch_size=self.n_batch)
